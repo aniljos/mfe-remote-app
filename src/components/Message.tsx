804235e250cd94370cd73f0796685338b0a75bc8
aniljos <anil.jos@gmail.com>
2024-06-13 06:58:22 +0530

    created

---
+++ src/components/Message.tsx
@@ -0,0 +1,13 @@
+
+
+function Message(){
+
+    return (
+        <div>
+            <h4>Message : MFE Remote</h4>
+            <p>Generated at {new Date().toLocaleString()}</p>
+        </div>
+    );
+}
+
+export default Message;
